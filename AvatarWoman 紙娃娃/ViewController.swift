a7be5cf0a6a956a81f4e00e24362ea2d6d9df9b9
Rose <qqbunny@icloud.com>
2021-04-23 16:18:45 +0800

    Initial Commit

---
+++ AvatarWoman 紙娃娃/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvatarWoman 紙娃娃
+//
+//  Created by Rose on 2021/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
